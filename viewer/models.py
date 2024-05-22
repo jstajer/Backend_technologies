daa1068cd7804d93537152a67a0492fbec2a0566
zodiac <jiristajer9@gmail.com>
2024-05-22 19:07:09 +0200

    novej push

--- viewer/models.py
+++ viewer/models.py
@@ -3,14 +3,14 @@ from django.db.models import *
 
 
 class Genre(Model):
-    name = CharField(max_length=16)
+    name = CharField(max_length=16, null=False, blank=False)
 
     def __str__(self):
         return f"<Genre: {self.name}"
 
 
 class Movie(Model):
-    title = CharField(max_length=128)
+    title = CharField(max_length=128, null=False, blank=False)
     genre = ForeignKey(Genre, on_delete=DO_NOTHING)
     rating = IntegerField()
     released = DateField()
