17345acff9344ce7c523e579a37b2b016bd1b7f4
zodiac <jiristajer9@gmail.com>
2024-05-23 19:59:32 +0200

    Další věci

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from viewer.models import *
+
 
 def hello(request):
     return HttpResponse("Hello World!")
@@ -12,3 +14,16 @@ def hello3(request):
     s = request.GET.get('s', '')
     return HttpResponse(f'Hello {s} World ! World!')
 
+def hello4(request):
+    adjectives = ['nice', 'cruel', 'beautiful', 'sunny']
+
+    return render(
+        request = request , #předáváme na další stránku request (obsahuje např. data o přihlášeném uživateli)
+
+        template_name="hello.html" ,
+        context={"adjectives": adjectives, 'name': 'Jirka' } )
+
+def movies(request):
+    movies = Movie.objects.all()
+    context = {'movies': movies}
+    return render(request, 'movies.html', context)
